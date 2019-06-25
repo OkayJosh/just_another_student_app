486d476d23e6f516befe3d6a8eac333fa92a187b
okayjosh <19627177+OkayJosh@users.noreply.github.com>
2019-06-25 16:44:56 +0100

    deployment settings

---
+++ app/urls.py
@@ -0,0 +1,89 @@
+from django.urls import path
+from .views import (HomeView, CreateStudentFormView, UpdateStudentFormView,
+                    DetailStudentView, CreateParentFormView, UpdateParentFormView,
+                    CreateBookFormView, UpdateBookFormView, CreateLiteracyFormView,
+                    UpdateLiteracyFormView, CreateIncentiveFormView, 
+                    UpdateIncentiveFormView, CreateAppraisalFormView,
+                    UpdateAppraisalFormView, CreateAttendanceFormView, 
+                    UpdateAttendanceFormView, CreatePostingFormView,
+                    UpdatePostingFormView, CreateAbsenceRecordFormView,
+                    UpdateAbsenceRecordFormView, CreateOutOfSchoolFormView,
+                    UpdateOutOfSchoolFormView, CreateReturnToSchoolFormView,
+                    UpdateReturnToSchoolFormView, CreateFeedingFormView,
+                    UpdateFeedingFormView, UserSignUpView)
+
+app_name = "student"
+
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+
+    path('signup', UserSignUpView.as_view(), name='signup'),
+
+    path('details/<int:pk>/<slug:slug>', DetailStudentView.as_view(), 
+    name='details'),
+
+    path('create/', CreateStudentFormView.as_view(), name='create'),
+
+    path('update/<int:pk>/<slug:slug>', UpdateStudentFormView.as_view(), 
+    name='update'),
+
+    path('parent/<int:pk>/<slug:slug>', CreateParentFormView.as_view(), 
+    name='parent'),
+
+    path('parent/update/<int:pk>/<slug:slug>', UpdateParentFormView.as_view(), 
+    name='parent_update'),
+
+    path('book/<int:pk>/<slug:slug>', CreateBookFormView.as_view(), 
+    name='book'),
+
+    path('book/update/<int:pk>/', UpdateBookFormView.as_view(), 
+    name='book_update'),
+
+    path('literacy/<int:pk>/<slug:slug>', CreateLiteracyFormView.as_view(), 
+    name='literacy'),
+
+    path('literacy/update/<int:pk>/<slug:slug>', UpdateLiteracyFormView.as_view(), 
+    name='literacy_update'),
+
+    path('incentive/<int:pk>/<slug:slug>', CreateIncentiveFormView.as_view(), 
+    name='incentive'),
+
+    path('incentive/update/<int:pk>/<slug:slug>', UpdateIncentiveFormView.as_view(), 
+    name='incentive_update'),
+
+    path('appraisal/<int:pk>/<slug:slug>', CreateAppraisalFormView.as_view(), 
+    name='appraisal'),
+
+    path('appraisal/update/<int:pk>/<slug:slug>', UpdateAppraisalFormView.as_view(), 
+    name='appraisal_update'),
+
+    path('attendance/<int:pk>/<slug:slug>', CreateAttendanceFormView.as_view(), 
+    name='attendance'),
+
+    path('attendance/update/<int:pk>/<slug:slug>', UpdateAttendanceFormView.as_view(), 
+    name='attendance_update'),
+
+    path('posting/<int:pk>/<slug:slug>', CreatePostingFormView.as_view(), 
+    name='posting'),
+
+    path('posting/update/<int:pk>/<slug:slug>', UpdatePostingFormView.as_view(), 
+    name='posting_update'),
+
+    path('absence/<int:pk>/<slug:slug>', CreateAbsenceRecordFormView.as_view(), 
+    name='absence'),
+
+    path('absence/update/<int:pk>/<slug:slug>', UpdateAbsenceRecordFormView.as_view(), 
+    name='absence_update'),
+
+    path('out/<int:pk>/<slug:slug>', CreateOutOfSchoolFormView.as_view(), 
+    name='out'),
+
+    path('out/update/<int:pk>/<slug:slug>', UpdateOutOfSchoolFormView.as_view(), 
+    name='out_update'),
+
+    path('return/<int:pk>/<slug:slug>', CreateReturnToSchoolFormView.as_view(), 
+    name='return'),
+
+    path('return/update/<int:pk>/<slug:slug>', UpdateReturnToSchoolFormView.as_view(), 
+    name='return_update'),
+]
---
+++ studentapp/settings/production.py
@@ -0,0 +1,15 @@
+from .base import *
+
+DEBUG = config('DEBUG', cast=bool)
+ALLOWED_HOSTS = ['.herokuapps.com', '.etentlabs.com']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT')
+    }
+}
