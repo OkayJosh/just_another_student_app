2264d0ef4753d3fcc39a384d289e2e34023d92db
okayjosh <19627177+OkayJosh@users.noreply.github.com>
2019-06-26 00:25:08 +0100

    modified

--- app/views.py
+++ app/views.py
@@ -59,6 +59,19 @@ class CreateStudentFormView(LoginRequiredMixin, CreateView):
                 self.object = form.save()
                 return redirect(self.get_success_url())
 
+        def get_success_url(self):
+                """Return the URL to redirect to after processing a valid form."""
+                if self.success_url:
+                        url = reverse(self.success_url.format(**self.object.__dict__))        
+                else:
+                        try:
+                                url = self.object.get_absolute_url()
+                        except AttributeError:
+                                raise ImproperlyConfigured(
+                                        "No URL to redirect to.  Either provide a url or define"
+                                        " a get_absolute_url method on the Model.")
+                return url
+
 class UpdateStudentFormView(LoginRequiredMixin, UpdateView):
         model = Student # The model is required alongside the "form_class"
         pk_url_kwarg = 'pk'
