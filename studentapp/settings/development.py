c47d870dca08b70e1a7cd04a9a62ffd54ba1d241
okayjosh <19627177+OkayJosh@users.noreply.github.com>
2019-06-25 22:31:34 +0100

    file restructuring

--- studentapp/settings/development.py
+++ studentapp/settings/development.py
@@ -1,7 +1,7 @@
 from .base import *
 
 DEBUG = True
-ALLOWED_HOSTS = ['stumas.herokuapp.com', 'stumas.etentlabs.com', '127.0.0.1']
+ALLOWED_HOSTS = ['stumas.herokuapp.com', 'stumas.etentlabs.com', '127.0.0.1', '0.0.0.0']
 
 DATABASES = {
     'default': {
